5e7e619454ee405ddb9355a29ec9205dd5b3ef8f
muneeb706 <muneeb706@gmail.com>
2020-04-23 17:23:40 -0500

    principal component analysis.

---
+++ R/pca.R
@@ -0,0 +1,48 @@
+
+# INSTALL AND LOAD PACKAGES ################################
+
+# Check if pacman is installed; if not, install it
+# install.packages("pacman")
+# install.packages("psych")
+pacman::p_load(psych)        # Load psych package
+pacman::p_depends(psych)     # Check dependencies for psych
+pacman::p_load(GPArotation)  # Install dependency
+
+# LOAD DATA ################################################
+
+# Read big5.csv from desktop
+b5 <- read.csv(paste(getwd(),"/data/linkedin_learning/b5.csv",sep=""), header = T) 
+colnames(b5)  # Get column names
+boxplot(b5)   # Boxplots for all Big 5 variables
+
+# PRINCIPAL COMPONENT ANALYSIS #############################
+
+# Can use "prcomp" or "princomp" but I prefer to do PCA
+# with the "principal" function from the "psych" package.
+
+# First PCA with no rotation, specify 5 factors
+pc0 <- principal(b5, nfactors = 5)
+pc0  # Check results
+
+# Second PCA with oblimin (oblique) rotation
+pc1 <- principal(b5, nfactors = 5, rotate = "oblimin")
+pc1  # Check results
+
+# INSPECT RESULTS ##########################################
+
+# Plot position of variables on components
+plot(pc1)
+
+# CLEAN UP #################################################
+
+# Clear workspace
+rm(list = ls()) 
+
+# Clear packages
+pacman::p_unload(psych, GPArotation)
+
+# Clear plots
+dev.off() 
+
+# Clear console
+cat("\014")  # ctrl+L
